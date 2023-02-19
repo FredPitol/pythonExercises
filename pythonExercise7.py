73c21d62852612f0ddf291d380328d4a198e90cd
Fred Pitol <61964787+FredPitol@users.noreply.github.com>
2023-02-19 05:28:55 -0300

    Calculating area of a square

---
+++ pythonExercise7.py
@@ -0,0 +1,11 @@
+print("Calculating area of a square\n")
+
+# User inputs
+S = float(input("Insert the value of the side of the square:\n"))
+M = str(input("Insert the unit of measure:\n"))
+
+# Processing data
+A = S * S
+
+# Result
+print(f"The area of the square is{A: .2f}{M}²")
