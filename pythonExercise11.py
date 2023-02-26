f2c71524be5fc896d5a459a96499762cedc49c7f
Frederico Pitol <freirefrederico1@gmail.com>
2023-02-26 12:50:10 -0300

    Calculate the volume of a sphere with the radius

---
+++ pythonExercise11.py
@@ -0,0 +1,14 @@
+import math
+
+# Calculate the volume in liters of a sphere
+# Insert the radius of the sphere
+R = float(input("Insert the radius of the sphere in centimeters:\n"))
+
+# Calculating the volume 
+V = 4 / 3 * math.pi * R ** 3
+
+# Round the number to the nearest tenth
+V = round(V, 1)
+
+# Printing the result
+print(f"The volume of the sphere is: {V: }cm³")
