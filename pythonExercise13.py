7d91bcd0be86573bf01711e73a636a0fdd9cf22c
Frederico Pitol <freirefrederico1@gmail.com>
2023-02-26 19:19:15 -0300

    Pseudocode

---
+++ pythonExercise13.py
@@ -0,0 +1,7 @@
+# Calculate and display the amount of paint (in cans) 
+# and the cost (in dollars) to paint a cylindrical tank
+# of circular base
+# 1 can = 5 liters
+# 1 liter paint 3 meters2
+# 1 can = 50 dollars 
+
